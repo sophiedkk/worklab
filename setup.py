9c3ebe168a4d55a773752095d4d0039d91a870d3
R de Klerk <rickdkk@gmail.com>
2020-10-21 13:14:28 +0200

    ignore_header on load_optitrack

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
 	name             		= 'worklab',
-	version          		= '1.6.3',
+	version          		= '1.6.4',
 	description      		= 'Basic scripts for worklab devices',
 	author           		= 'Rick de Klerk',
 	author_email     		= 'r.de.klerk@umcg.nl',
